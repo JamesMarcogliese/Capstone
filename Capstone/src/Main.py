22a89b83f3332471f4ccf8c14d083de2d5fc8c0a
JamesMarcogliese <james.marcogliese@gmail.com>
2014-08-24 20:42:41 -0400

    Add main

---
+++ Capstone/src/Main.py
@@ -0,0 +1,24 @@
+'''
+Created on Aug 24, 2014
+
+@author: James
+'''
+#!/usr/bin/python
+#import Adafruit_BBIO.GPIO as GPIO
+# init code here. Setup components if needed 
+
+verified = False
+
+
+if __name__ == '__main__':
+    pass
+    
+def enroll ():
+
+def encrypt ():
+
+def decrypt ():
+
+def delete ():
+
+def verify ():
