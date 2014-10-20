ce24b3392dec3666c9582655493d1461dc582984
JamesMarcogliese <james.marcogliese@gmail.com>
2014-10-20 15:54:45 -0400

    push lib

---
+++ Capstone/Lib/FPS/enrolar.py
@@ -0,0 +1,18 @@
+'''
+Created on 08/04/2014
+
+@author: jeanmachuca
+'''
+import FPS, sys
+from test_raw import *
+
+if __name__ == '__main__':
+    fps = FPS.FPS_GT511C3()
+    fps.UseSerialDebug = True
+   # fps.SetLED = True
+    fps.SetLED()	
+				
+#    Enroll(fps,sys.argv[1])
+    fps.Close()
+    pass
+
