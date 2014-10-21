4d26050d1d9e30178e97ec0f663b0e4b8e1a2612
JamesMarcogliese <james.marcogliese@gmail.com>
2014-10-21 16:56:04 -0400

    save

--- Capstone/src/Main.py
+++ Capstone/src/Main.py
@@ -23,19 +23,16 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_c
 
 kp = BBb_GPIO.keypad(columnCount = 4)
 
-
-if __name__ == '__main__':
-    
+#if __name__ == '__main__':    
 while (1)
 lcd.message('Hello World!')	
 
 
 
 
-pass
+#pass
 
-while_stmt ::=  "while" expression ":" suite
-                
+            
                 
                 
                 
