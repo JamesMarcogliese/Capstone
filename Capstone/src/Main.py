0a2b257542175413a2610ed8c4499c9dd0138fc1
JamesMarcogliese <james.marcogliese@gmail.com>
2014-10-22 23:11:06 -0400

    Reformated code to meet conventions pass1

--- Capstone/src/Main.py
+++ Capstone/src/Main.py
@@ -4,14 +4,16 @@ Created on Aug 24, 2014
 @author: James
 '''
 #!/usr/bin/python
-#import Adafruit_BBIO.GPIO as GPIO
-import math, time, sys
+import time
 import Adafruit_CharLCD as LCD
 from matrix_keypad import BBb_GPIO
+import Adafruit_BBIO.UART as UART
 import FPS
-from test_raw import *
+import test_raw
 
+UART.setup("UART2")
 fps = FPS.FPS_GT511C3()
+fps.open()
 
 lcd_rs        = 'P8_8'
 lcd_en        = 'P8_10'
@@ -23,11 +25,12 @@ lcd_backlight = 'P8_7'
 lcd_columns = 20
 lcd_rows    = 4
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
 kp = BBb_GPIO.keypad(columnCount = 3)
-loop = 1
+
 ID = 0
-def digit():
-    # Loop while waiting for a keypress
+
+def digit(): # Loop while waiting for a keypress
     r = None
     while r == None:
         r = kp.getKey()
@@ -35,44 +38,47 @@ def digit():
 
 def enrollID():
     fps.UseSerialDebug = True
-	enrollCount = fps.GetEnrollCount()
+    if (fps.GetEnrollCount() == 19):
+        lcd.message('Device at maximum\ncapacity. Please\ndelete users.')
+        time.sleep(3.0)
+        break
     lcd.clear()
-    fps.SetLED(True)							#check IDS and check if exists already
-	lcd.message("Press finger on\nscanner to check ID")
-     while (fps.IsPressFinger() == False)
-	  time.sleep(0.1)
-      bret = fps.CaptureFinger(True)
-	  if (bret != False) 
+    fps.SetLED(True)							
+    lcd.message('Press finger on\nscanner to check if\nalready enrolled.')
+    while (fps.IsPressFinger() == False):
+        time.sleep(0.1)
+    bret = fps.CaptureFinger(True)
+    if (bret != False):
+        lcd.clear()             # break off into verify def
+        lcd.message('Remove finger.\n')
+    ID = fps.Identify1_N()
+    if (ID != 200):
         lcd.clear()
-        lcd.message('Remove finger\n')
-	ID = fps.Identify1_N()
-	if (ID != 200)
-	  lcd.clear()
-	  lcd.message('You are already\nenrolled. Select\nanother option.\nReturning to main.')
-	  time.sleep(3.0)
-	  break
-	else
-	  for i in range(0,19):
-        if (fps.CheckEnrolled(i) == False)
-          ID = i		  
-	lcd.message('Press finger to\nEnroll')
-	fps.EnrollStart()
-    while (fps.IsPressFinger() == False)
-	  time.sleep(0.1)
-      bret = fps.CaptureFinger(True)
-	  iret = 0
-	  if (bret != False) 
+        lcd.message('You are already\nenrolled. Returning\nto main.')
+        time.sleep(3.0)
+        break
+    else:
+        for i in range(0,19):
+            if (fps.CheckEnrolled(i) == False):
+                ID = i		  
+    lcd.message('Press finger to\nEnroll.')
+    fps.EnrollStart(ID)
+    while (fps.IsPressFinger() == False):
+        time.sleep(0.1)
+    bret = fps.CaptureFinger(True)
+    iret = 0
+    if (bret != False):
         lcd.clear()
-        lcd.message('Remove finger\n')
+        lcd.message('Remove finger.\n')
 		fps.
     lcd.clear()
-    lcd.message('Press same finger again\n')
+    lcd.message('Press same finger again.\n')
     lcd.clear()
-    lcd.message('Remove finger\n')
+    lcd.message('Remove finger.\n')
     lcd.clear()
-    lcd.message('Press same finger\nyet again\n')
+    lcd.message('Press same finger\nyet again.\n')
     lcd.clear()
-    lcd.message('Remove finger\n')
+    lcd.message('Remove finger.\n')
     lcd.clear()
 	fps.SetLED(False)
 def encrypt():
@@ -81,26 +87,25 @@ def decrypt():
     print "decrypt"
 def deleteID():                
     print "delete" 
- 
+def verify():
+    
 
 #MAIN
-import Adafruit_BBIO.UART as UART
-UART.setup("UART2")
 
-while (loop):
+while True:
     lcd.clear()
     lcd.message('  PUD-E MAIN MENU\n1-Enroll   2-Encrypt\n3-Decrypt  4-Delete\n  <SELECT OPTION>')
     d1 = digit()
     print d1
     if d1 == 1:
-	enrollID()
+        enrollID()
     elif d1 == 2:
-	encrypt()
+        encrypt()
     elif d1 == 3:
-	decrypt()
+        decrypt()
     elif d1 == 4:
-	deleteID()
+        deleteID()
     elif d1 > 4 or d1 == 0 or d1 == "*" or d1 == "#":
-	pass
+        pass
 
   
